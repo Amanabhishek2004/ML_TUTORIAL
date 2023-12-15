1a6db97607c09a649e471920555d955f60144a3f
AMAN <amanabhishek2004@gmail.com>
2023-12-15 18:58:17 +0530

    delted some random datasets

--- ML_MAIN/SQL_DATA.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-directory_path = r"C:\Users\AMAN\Pictures\Screenshots"
-
-# Using 'explorer' command to open the directory in the file explorer
-subprocess.Popen(['explorer', directory_path], shell=True)
--- ML_MAIN/data.py
+++
@@ -1,6 +0,0 @@
-#  for reading csv files
-
-import pandas as pd
-
-df = pd.read_csv('gdppercapita_us_inflation_adjusted.csv').head(2)
-print(df)
